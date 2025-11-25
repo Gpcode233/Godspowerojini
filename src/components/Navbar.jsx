7ce6fd2a4f6fb8c01a8041b7e11bf8871700772a
Gpcode233 <godspowerojini8@gmail.com>
2025-11-25 16:03:53 +0100

    feat: Initialize macOS-themed portfolio project with core components, assets, and configuration.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { navLinks, navIcons } from '#constants'
+import React from 'react'
+import dayjs from 'dayjs'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className='font-bold'>Godspower's Portfolio</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img src={img} className='icon-hover' alt={`icon-${id}`} />
+                        </li>
+                    ))}
+                </ul>
+
+                <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+            </div>
+        </nav>
+    )
+}
