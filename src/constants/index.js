9181cdc42e53fcaa6b78408c2d92218e85dbe991
Gpcode233 <godspowerojini8@gmail.com>
2025-12-02 10:55:05 +0100

    updated the blog

--- src/constants/index.js
+++ src/constants/index.js
@@ -79,23 +79,23 @@ const blogPosts = [
     id: 1,
     date: "Sep 2, 2025",
     title:
-      "TypeScript Explained: What It Is, Why It Matters, and How to Master It",
-    image: "/images/blog1.png",
-    link: "https://jsmastery.com/blog/typescript-explained-what-it-is-why-it-matters-and-how-to-master-it",
+      "TypeScript Explained: What It Is, How to Master It, and Why It Matters",
+    image: "/images/blog1.jpg",
+    link: "https://medium.com/@godspowerojini8/typescript-explained-what-it-is-how-to-master-it-and-why-it-matters-d223f8078a44",
   },
+  // {
+  //   id: 2,
+  //   date: "Aug 28, 2025",
+  //   title: "The Ultimate Guide to Mastering Three.js for 3D Development",
+  //   image: "/images/blog2.png",
+  //   link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-three-js-for-3d-development",
+  // },
   {
     id: 2,
-    date: "Aug 28, 2025",
-    title: "The Ultimate Guide to Mastering Three.js for 3D Development",
-    image: "/images/blog2.png",
-    link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-three-js-for-3d-development",
-  },
-  {
-    id: 3,
     date: "Aug 15, 2025",
-    title: "The Ultimate Guide to Mastering GSAP Animations",
+    title: "The Complete Guide to Mastering GSAP Animations",
     image: "/images/blog3.png",
-    link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-gsap-animations",
+    link: "https://medium.com/@godspowerojini8/the-completeguide-to-mastering-gsap-animations-88e4ed11b7c0",
   },
 ];
 
